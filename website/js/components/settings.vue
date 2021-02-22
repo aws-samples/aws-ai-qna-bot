1999a3b3f3577c5b8e62c8daf0e6814da57695c4
Karl Thomas <thokrl+github@amazon.com>
2021-02-21 21:51:34 -0500

    merged feature-addSettings

--- website/js/components/settings.vue
+++ website/js/components/settings.vue
@@ -14,6 +14,19 @@
             v-btn(@click="SaveSettings") Save
             //- v-btn Add New parameter
             v-btn(@click="resetToDefaults") Reset to defaults
+    
+    v-btn(bottom fab fixed right @click="showAddModal = true")
+        v-icon(color ="primary") add
+    v-dialog(v-model ="showAddModal")
+        v-card
+            v-card-title New Setting
+            v-card-text
+                v-text-field(label="Name" v-model="newKey")
+                v-text-field(label="Value" v-model="newValue")
+            v-card-actions
+                v-spacer
+                v-btn(@click="addSetting") Add
+                v-btn(@click="closeModal") Cancel
             
 </template>
 
@@ -38,11 +51,14 @@ module.exports = {
     data: function () {
         var self = this
         return {
+            showAddModal: false,
             mergedSettings: {},
             defaultSettings: {},
             customSettings: {},
             settingsHolder: {},
-            successAlert: false
+            successAlert: false,
+            newKey: "",
+            newValue: ""
         }
     },
     components: {},
@@ -93,6 +109,25 @@ module.exports = {
                 window.scrollTo(0, 0);
             }
 
+        },
+        showModal: async function() {
+            this.showAddModal = true;
+        },
+        closeModal: async function() {
+            this.showAddModal = false;
+        },
+        addSetting: async function() {
+            if (this.newKey.length >= 1) {
+                console.log('hi tom');
+                this.mergedSettings[this.newKey] = this.newValue;
+                this.customSettings[this.newKey] = this.newValue;
+                this.settingsHolder[this.newKey] = this.newValue;
+            }
+            console.log(this.mergedSettings);
+
+            this.showAddModal = false;
+            this.newValue = "";
+            this.newKey = "";
         }
     }
 }
